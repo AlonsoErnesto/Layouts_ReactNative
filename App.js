b64786419b6217663cd6cf7e33e13f3a29c10b52
AlonsoErnesto <ernesto134alonso123@gmail.com>
2022-10-31 21:43:01 -0500

    Layouts-row-colums

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar} from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.view}>
+      <StatusBar/>
+      <View style={styles.view2}></View>
+      <View style={styles.view3}></View>
+      <View style={styles.view4}></View>
+      <View style={styles.view5}></View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  //PADRE
+  view:{
+    // flex:1,
+    // minHeight:'100%' 
+    // flexDirection:'row',     // ↔
+    // justifyContent:'center', // ↔  Horizontal
+    // alignItems:'center'      // ↕ Vertical
+
+    flex:1,
+    flexDirection:'column' ,   // Por defecto ↕
+    alignItems:'center',       // ↔  Horizontal
+    justifyContent:'center',   // ↕ Vertical
+  },
+
+  // HIJOS
+
+  view2:{
+    padding:20,
+    backgroundColor:'red',
+  },
+  view3:{
+    padding:20,
+    backgroundColor:'yellow',
+  },
+  view4:{
+    padding:20,
+    backgroundColor:'blue',
+  },
+  view5:{
+    padding:20,
+    backgroundColor:'green',
   },
 });
